e6fc2d991b94aa9ae337f1f4686fca6fa354a5ff
Ed Lomonaco <2343334+eman1986@users.noreply.github.com>
2019-06-08 23:32:05 -0400

    initial work

---
+++ src/Annotations/BsonIgnore.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file is part of the mongodb-helpers package.
+ *
+ * (c) 2019 Eman Development & Design
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Edd\MongoDbHelpers\Annotations;
+
+/**
+ * Class BsonIgnore
+ * @package Edd\MongoDbHelpers\Annotations
+ * @Annotation
+ * @Target({"PROPERTY"})
+ */
+final class BsonIgnore
+{
+    // todo:
+}
